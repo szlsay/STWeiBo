41aa83651634e62732e41c6143b519449153acca
shenzhaoliang <409178030@qq.com>
2015-11-14 12:20:58 +0800

    03-STWeiBo-命名空间 2015-11-14

--- STWeiBo/STWeiBo/Classes/Main/MainViewController.swift
+++ STWeiBo/STWeiBo/Classes/Main/MainViewController.swift
@@ -21,33 +21,13 @@ class MainViewController: UITabBarController {
         // 注意: 在iOS7以前如果设置了tintColor只有文字会变, 而图片不会变
         tabBar.tintColor = UIColor.orangeColor()
         
-        /*
-        // 1.创建首页
-        let home = HomeTableViewController()
-        // 1.1设置首页tabbar对应的数据
-        home.tabBarItem.image = UIImage(named: "tabbar_home")
-        home.tabBarItem.selectedImage = UIImage(named: "tabbar_home_highlighted")
-        //        home.tabBarItem.title = "首页"
-        
-        // 1.2设置导航条对应的数据
-        //        home.navigationItem.title = "首页"
-        home.title = "首页"
-        
-        // 2.给首页包装一个导航控制器
-        let nav = UINavigationController()
-        nav.addChildViewController(home)
-        
-        // 3.将导航控制器添加到当前控制器上
-        addChildViewController(nav)
-        */
-        
-        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(DiscoverTableViewController(), title: "广场", imageName: "tabbar_discover")
-        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+        // 2.添加子控制器
+        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
+        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
+        addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
         
     }
-    
     /**
      初始化子控制器
      
@@ -55,20 +35,39 @@ class MainViewController: UITabBarController {
      :param: title           子控制器的标题
      :param: imageName       子控制器的图片
      */
-    private func addChildViewController(childController: UIViewController, title:String, imageName:String) {
+     //    private func addChildViewController(childController: UIViewController, title:String, imageName:String) {
+    
+    private func addChildViewController(childControllerName: String, title:String, imageName:String) {
+        // <DSWeibo.HomeTableViewController: 0x7ff3a15967e0>
+        //        print(childController)
+        
+        // -1.动态获取命名空间
+        let ns = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        
+        // 0 .将字符串转换为类
+        // 0.1默认情况下命名空间就是项目的名称, 但是命名空间名称是可以修改的
+        let cls:AnyClass? = NSClassFromString(ns + "." + childControllerName)
+        // 0.2通过类创建对象
+        // 0.2.1将AnyClass转换为指定的类型
+        let vcCls = cls as! UIViewController.Type
+        // 0.2.2通过class创建对象
+        let vc = vcCls.init()
+        
+        // 0.1通过类创建一个对象
+        
         
         // 1设置首页对应的数据
-        childController.tabBarItem.image = UIImage(named: imageName)
-        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
-        childController.title = title
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        vc.title = title
         
         // 2.给首页包装一个导航控制器
         let nav = UINavigationController()
-        nav.addChildViewController(childController)
+        nav.addChildViewController(vc)
         
         // 3.将导航控制器添加到当前控制器上
         addChildViewController(nav)
+        
     }
     
 }
-
---
+++ STWeiBo/STWeiBo/Classes/Main/Resource/NullViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NullViewController.swift
+//  STWeiBo
+//
+//  Created by ST on 15/11/14.
+//  Copyright © 2015年 ST. All rights reserved.
+//
+
+import UIKit
+
+class NullViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
