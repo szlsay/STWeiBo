074cc5c1526ff5da2976353701575daebf6f7f67
shenzhaoliang <409178030@qq.com>
2015-11-14 17:31:20 +0800

    16-STWeiBo-定位二维码

--- STWeiBo/STWeiBo/Classes/Home/QRCode/QRCodeViewController.swift
+++ STWeiBo/STWeiBo/Classes/Home/QRCode/QRCodeViewController.swift
@@ -79,6 +79,9 @@ class QRCodeViewController: UIViewController, UITabBarDelegate {
         // 添加预览图层
         view.layer.insertSublayer(previewLayer, atIndex: 0)
         
+        // 添加绘制图层到预览图层上
+        previewLayer.addSublayer(drawLayer)
+        
         // 6.告诉session开始扫描
         session.startRunning()
     }
@@ -152,17 +155,104 @@ class QRCodeViewController: UIViewController, UITabBarDelegate {
         return layer
     }()
     
+    // 创建用于绘制边线的图层
+    private lazy var drawLayer: CALayer = {
+        let layer = CALayer()
+        layer.frame = UIScreen.mainScreen().bounds
+        return layer
+    }()
+    
 }
+
 
 extension QRCodeViewController: AVCaptureMetadataOutputObjectsDelegate
 {
     // 只要解析到数据就会调用
     func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!)
     {
+        // 0.清空图层
+        clearConers()
+        
+        // 1.获取扫描到的数据
         // 注意: 要使用stringValue
-        print(metadataObjects.last?.stringValue)
+        //        print(metadataObjects.last?.stringValue)
         resultLabel.text = metadataObjects.last?.stringValue
         resultLabel.sizeToFit()
+        
+        // 2.获取扫描到的二维码的位置
+        //        print(metadataObjects.last)
+        // 2.1转换坐标
+        for object in metadataObjects
+        {
+            // 2.1.1判断当前获取到的数据, 是否是机器可识别的类型
+            if object is AVMetadataMachineReadableCodeObject
+            {
+                // 2.1.2将坐标转换界面可识别的坐标
+                let codeObject = previewLayer.transformedMetadataObjectForMetadataObject(object as! AVMetadataObject) as! AVMetadataMachineReadableCodeObject
+                //                print(codeObject)
+                // 2.1.3绘制图形
+                drawCorners(codeObject)
+            }
+        }
+    }
+    
+    /**
+     绘制图形
+     
+     :param: codeObject 保存了坐标的对象
+     */
+    private func drawCorners(codeObject: AVMetadataMachineReadableCodeObject)
+    {
+        if codeObject.corners.isEmpty
+        {
+            return
+        }
+        
+        // 1.创建一个图层
+        let layer = CAShapeLayer()
+        layer.lineWidth = 4
+        layer.strokeColor = UIColor.redColor().CGColor
+        layer.fillColor = UIColor.clearColor().CGColor
+        
+        // 2.创建路径
+        //        layer.path = UIBezierPath(rect: CGRect(x: 100, y: 100, width: 200, height: 200)).CGPath
+        let path = UIBezierPath()
+        var point = CGPointZero
+        var index: Int = 0
+        // 2.1移动到第一个点
+        //        print(codeObject.corners.last)
+        // 从corners数组中取出第0个元素, 将这个字典中的x/y赋值给point
+        CGPointMakeWithDictionaryRepresentation((codeObject.corners[index++] as! CFDictionaryRef), &point)
+        path.moveToPoint(point)
+        
+        // 2.2移动到其它的点
+        while index < codeObject.corners.count
+        {
+            CGPointMakeWithDictionaryRepresentation((codeObject.corners[index++] as! CFDictionaryRef), &point)
+            path.addLineToPoint(point)
+        }
+        // 2.3关闭路径
+        path.closePath()
+        
+        // 2.4绘制路径
+        layer.path = path.CGPath
+        
+        // 3.将绘制好的图层添加到drawLayer上
+        drawLayer.addSublayer(layer)
+    }
+    /**
+     清空边线
+     */
+    private func clearConers(){
+        // 1.判断drawLayer上是否有其它图层
+        if drawLayer.sublayers == nil || drawLayer.sublayers?.count == 0{
+            return
+        }
+        
+        // 2.移除所有子图层
+        for subLayer in drawLayer.sublayers!
+        {
+            subLayer.removeFromSuperlayer()
+        }
     }
 }
-
