79952e59626eb804d553ecdd16163234a799ba59
shenzhaoliang <409178030@qq.com>
2015-11-16 15:31:18 +0800

    30-STWeiBo-判断新版本

--- STWeiBo/STWeiBo/AppDelegate.swift
+++ STWeiBo/STWeiBo/AppDelegate.swift
@@ -36,6 +36,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // 2.创建根控制器
         window?.rootViewController = WelcomeViewController()
         window?.makeKeyAndVisible()
+        
+        print(isNewupdate())
     }
+    
+    private func isNewupdate() -> Bool{
+        // 1.获取当前软件的版本号 --> info.plist
+        let currentVersion = NSBundle.mainBundle().infoDictionary!["CFBundleShortVersionString"] as! String
+        
+        // 2.获取以前的软件版本号 --> 从本地文件中读取(以前自己存储的)
+        let sandboxVersion =  NSUserDefaults.standardUserDefaults().objectForKey("CFBundleShortVersionString") as? String ?? ""
+        
+        //        var temp = nil ?? "123"
+        //        print(temp)
+        
+        print("current = \(currentVersion) sandbox = \(sandboxVersion)")
+        
+        // 3.比较当前版本号和以前版本号
+        //   2.0                    1.0
+        if currentVersion.compare(sandboxVersion) == NSComparisonResult.OrderedDescending
+        {
+            // 3.1如果当前>以前 --> 有新版本
+            // 3.1.1存储当前最新的版本号
+            // iOS7以后就不用调用同步方法了
+            NSUserDefaults.standardUserDefaults().setObject(currentVersion, forKey: "CFBundleShortVersionString")
+            return true
+        }
+        
+        // 3.2如果当前< | ==  --> 没有新版本
+        return false
+    }
+    
 }
+
 
