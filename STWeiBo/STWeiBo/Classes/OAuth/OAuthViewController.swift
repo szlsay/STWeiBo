33d89a78445a0685a2c97b8c174c7a76e23521cf
shenzhaoliang <409178030@qq.com>
2015-11-16 11:03:32 +0800

    20-STWeiBo-获取已经授权RequestToken

--- STWeiBo/STWeiBo/Classes/OAuth/OAuthViewController.swift
+++ STWeiBo/STWeiBo/Classes/OAuth/OAuthViewController.swift
@@ -41,6 +41,54 @@ class OAuthViewController: UIViewController {
     // MARK: - 懒加载
     private lazy var webView: UIWebView = {
         let wv = UIWebView()
+        wv.delegate = self
         return wv
     }()
+}
+
+
+extension OAuthViewController: UIWebViewDelegate
+{
+    // 返回ture正常加载 , 返回false不加载
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool
+    {
+        /*
+        加载授权界面: https://api.weibo.com/oauth2/authorize?client_id=2624860832&redirect_uri=http://www.520it.com
+        
+        跳转到授权界面: https://api.weibo.com/oauth2/authorize
+        
+        授权成功: http://www.520it.com/?code=91e779d15aa73698cbbb72bc7452f3b3
+        
+        取消授权: http://www.520it.com/?error_uri=%2Foauth2%2Fauthorize&error=access_denied&error_description=user%20denied%20your%20request.&error_code=21330
+        */
+        print(request.URL?.absoluteString)
+        
+        // 1.判断是否是授权回调页面, 如果不是就继续加载
+        let urlStr = request.URL!.absoluteString
+        if !urlStr.hasPrefix(WB_redirect_uri)
+        {
+            // 继续加载
+            return true
+        }
+        
+        // 2.判断是否授权成功
+        let codeStr = "code="
+        if request.URL!.query!.hasPrefix(codeStr)
+        {
+            // 授权成功
+            print("授权成功")
+            // 取出已经授权的RequestToken
+            // codeStr.endIndex是拿到code=最后的位置
+            let code = request.URL!.query?.substringFromIndex(codeStr.endIndex)
+            print(code)
+        }else
+        {
+            // 取消授权
+            print("取消授权")
+            // 关闭界面
+            close()
+        }
+        
+        return false
+    }
 }
