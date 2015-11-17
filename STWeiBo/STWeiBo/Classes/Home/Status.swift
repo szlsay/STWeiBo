f1fab437ba8cee52f85e6389e1b3003cb6556584
shenzhaoliang <409178030@qq.com>
2015-11-17 11:46:54 +0800

    41-STWeiBo-缓存配图

--- STWeiBo/STWeiBo/Classes/Home/Status.swift
+++ STWeiBo/STWeiBo/Classes/Home/Status.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import SDWebImage
 class Status: NSObject {
     /// 微博创建时间
     var created_at: String?
@@ -47,6 +47,24 @@ class Status: NSObject {
 
     /// 配图数组
     var pic_urls: [[String: AnyObject]]?
+        {
+        didSet{
+            // 1.初始化数组
+            storedPicURLS = [NSURL]()
+            // 2遍历取出所有的图片路径字符串
+            for dict in pic_urls!
+            {
+                if let urlStr = dict["thumbnail_pic"]
+                {
+                    // 将字符串转换为URL保存到数组中
+                    storedPicURLS?.append(NSURL(string: urlStr as! String)!)
+                }
+            }
+        }
+    }
+    
+    /// 保存当前微博所有配图的URL
+    var storedPicURLS: [NSURL]?
     
     /// 用户信息
     var user: User?
@@ -62,8 +80,11 @@ class Status: NSObject {
             // 2.遍历数组, 将字典转换为模型
             let models = dict2Model(JSON["statuses"] as! [[String: AnyObject]])
 //            print(models)
+            
+            // 3.缓存微博配图
+            cacheStatusImages(models, finished: finished)
             // 2.通过闭包将数据传递给调用者
-            finished(models: models, error: nil)
+//            finished(models: models, error: nil)
             
             }) { (_, error) -> Void in
                 print(error)
@@ -71,6 +92,40 @@ class Status: NSObject {
                 
         }
     }
+    
+    
+    class func cacheStatusImages(list: [Status], finished: (models:[Status]?, error:NSError?)->()) {
+        
+        // 1.创建一个组
+        let group = dispatch_group_create()
+        
+        // 1.缓存图片
+        for status in list
+        {
+            for url in status.storedPicURLS!
+            {
+                // 将当前的下载操作添加到组中
+                dispatch_group_enter(group)
+                
+                // 缓存图片
+                SDWebImageManager.sharedManager().downloadImageWithURL(url, options: SDWebImageOptions(rawValue: 0), progress: nil, completed: { (_, _, _, _, _) -> Void in
+                    
+                    // 离开当前组
+                    dispatch_group_leave(group)
+                    //                    print("OK")
+                })
+            }
+        }
+        
+        // 2.当所有图片都下载完毕再通过闭包通知调用者
+        dispatch_group_notify(group, dispatch_get_main_queue()) { () -> Void in
+            //            print("Over")
+            // 能够来到这个地方, 一定是所有图片都下载完毕
+            finished(models: list, error: nil)
+        }
+    }
+    
+
     
     /// 将字典数组转换为模型数组
     class func dict2Model(list: [[String: AnyObject]]) -> [Status] {
