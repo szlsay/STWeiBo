60039827d3ea5caa10fc6514d11b72609c27b1a4
shenzhaoliang <409178030@qq.com>
2015-11-19 17:51:17 +0800

    49-STWeiBo-KVO监听刷新

--- STWeiBo/STWeiBo/Classes/Home/HomeRefreshControl.swift
+++ STWeiBo/STWeiBo/Classes/Home/HomeRefreshControl.swift
@@ -23,11 +23,48 @@ class HomeRefreshControl: UIRefreshControl {
         
         // 2.布局子控件
         refreshView.ST_AlignInner(type: ST_AlignType.Center, referView: self, size: CGSize(width: 170, height: 60))
+        
+        /*
+        1.当用户下拉到一定程度的时候需要旋转箭头
+        2.当用户上推到一定程度的时候需要旋转箭头
+        3.当下拉刷新控件触发刷新方法的时候, 需要显示刷新界面(转轮)
+        
+        通过观察:
+        越往下拉: 值就越小
+        越往上推: 值就越大
+        */
+
+        addObserver(self, forKeyPath: "frame", options: NSKeyValueObservingOptions.New, context: nil)
     }
+    
+    /// 定义变量记录是否需要旋转监听
+    private var rotationArrow = false
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        print("\(__FUNCTION__) \(frame.origin.y)")
+        
+        if frame.origin.y >= 0 {
+            return
+        }
+        
+        if frame.origin.y >= -50 && !rotationArrow
+        {
+            print("翻转")
+            rotationArrow = true
+        }else if frame.origin.y < -50 && rotationArrow
+        {
+            print("翻转回来")
+            rotationArrow = false
+        }
+    }
+    
     
     // MARK: - 懒加载
     private lazy var refreshView : HomeRefreshView =  HomeRefreshView.refreshView()
     
+    deinit
+    {
+        removeObserver(self, forKeyPath: "frame")
+    }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
