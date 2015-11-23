ceefb00eb17505f983908254c4d2da29a3a29e94
shenzhaoliang <409178030@qq.com>
2015-11-23 09:43:10 +0800

    57-STWeiBo-长短图计算

--- STWeiBo/STWeiBo/Classes/Home/PhotoBrowser/PhotoBrowserCell.swift
+++ STWeiBo/STWeiBo/Classes/Home/PhotoBrowser/PhotoBrowserCell.swift
@@ -24,12 +24,39 @@ class PhotoBrowserCell: UICollectionViewCell {
                 3.长图显示也会有一些问题
                 */
 //                self.iconView.frame = CGRect(origin: CGPointZero, size: image.size)
-                let size = self.displaySize(image)
-                self.iconView.frame = CGRect(origin: CGPointZero, size: size)
+//                let size = self.displaySize(image)
+//                self.iconView.frame = CGRect(origin: CGPointZero, size: size)
+                self.setImageViewPostion()
+                
             }
         }
     }
     
+    /**
+     调整图片显示的位置
+     */
+    private func setImageViewPostion()
+    {
+        // 1.拿到按照宽高比计算之后的图片大小
+        let size = self.displaySize(iconView.image!)
+        // 2.判断图片的高度, 是否大于屏幕的高度
+        if size.height < UIScreen.mainScreen().bounds.height
+        {
+            // 2.2小于 短图 --> 设置边距, 让图片居中显示
+            iconView.frame = CGRect(origin: CGPointZero, size: size)
+            // 处理居中显示
+            let y = (UIScreen.mainScreen().bounds.height - size.height) * 0.5
+            self.scrollview.contentInset = UIEdgeInsets(top: y, left: 0, bottom: y, right: 0)
+        }else
+        {
+            // 2.1大于 长图 --> y = 0, 设置scrollview的滚动范围为图片的大小
+            iconView.frame = CGRect(origin: CGPointZero, size: size)
+            scrollview.contentSize = size
+        }
+    }
+    /**
+     按照图片的宽高比计算图片显示的大小
+     */
     private func displaySize(image: UIImage) -> CGSize
     {
         // 1.拿到图片的宽高比
@@ -52,7 +79,7 @@ class PhotoBrowserCell: UICollectionViewCell {
     {
         // 1.添加子控件
         contentView.addSubview(scrollview)
-        contentView.addSubview(iconView)
+        scrollview.addSubview(iconView)
         
         // 2.布局子控件
         scrollview.frame = UIScreen.mainScreen().bounds
